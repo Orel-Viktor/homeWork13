9a2ec2703c3cf3f2e97d699a139d1c4daaa02782
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-11 16:46:35 +0300

    home-wrok13

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-import './style.css'
+"use strict";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import "./styles/main.scss";
+
+const _prev = document.querySelector('.js--prev')
+const _next = document.querySelector('.js--next')
+const _list = document.querySelector('.js--list')
+
+
+_next.addEventListener('click', function () {
+  const active = _list.querySelector('.active')
+  const _nextElement = active.nextElementSibling
+  if (_nextElement) {
+    _prev.classList.remove('half-opacity')
+    active.classList.remove('active')
+    _nextElement.classList.add('active')
+  }
+  if (!_nextElement){
+    _next.classList.add('half-opacity')
+  }
+})
+
+_prev.addEventListener('click', function () {
+  const active = _list.querySelector('.active')
+  const _prevElement = active.previousElementSibling
+
+  if (_prevElement) {
+    _next.classList.remove('half-opacity')
+    active.classList.remove('active')
+    _prevElement.classList.add('active')
+  }
+  if (!_prevElement) {
+    _prev.classList.add('half-opacity')
+  }
+  { }
+})
+
+
