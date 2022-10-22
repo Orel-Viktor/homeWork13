df406916672ce3a6d787d9818a1e2216fad72109
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-22 15:09:49 +0300

    password

--- main.js
+++ main.js
@@ -68,18 +68,44 @@ import "./styles/main.scss";
 function Validation(form) {
   const myForm = document.querySelector(form);
   const elements = myForm.elements;
-  const parrentItemClass = "form-control";
-
-  myForm.addEventListener('submit', (event) => {
+  const parrentItemClass = "js--form-contorl";
+  const errorBorderClass = "error-border";
+  // const parrentItemClass = "form-control";
+  myForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    this.chekFormElemnt() 
+    this.chekFormElemnt();
   });
 
-  this.chekFormElemnt = function() {
-    for (let i = 0; i < elements.lenght; i++) {
+  this.chekFormElemnt = function () {
+    for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
       const passwordMessage = element.dataset.password;
-      console.log(element)
+      console.log(passwordMessage);
+      if (passwordMessage) {
+        this.validPassword(passwordMessage);
+      }
+    }
+  };
+
+  this.validPassword = function (message) {
+    const allPasswordElements = myForm.querySelectorAll("input[type=password]");
+    console.log(allPasswordElements);
+    const valueArr = Array.from(allPasswordElements).map(
+      (element) => element.value
+    );
+    console.log(valueArr);
+    if (valueArr[0] !== valueArr[1]) {
+      allPasswordElements.forEach((item) => this.errorTemplate(item, message));
+      return
+    }
+  };
+
+  this.errorTemplate = function (element, message) {
+    const parent = element.closest(`.${parrentItemClass}`);
+    console.log(parent);
+    if (!parent.classList.contains(errorBorderClass)) {
+      parent.classList.add(errorBorderClass);
+      parent.insertAdjacentHTML("beforeend", `<small>${message}</small>`);
     }
   };
 }
